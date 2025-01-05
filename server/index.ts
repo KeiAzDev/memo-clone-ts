11d4f5af33fbceef5d1870b2aa44399c9f90f128
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-04 16:47:59 -0800

    dotenv追加、dbとconnect完了

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import env from 'dotenv';
+env.config();
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
-});
+if (!process.env.MONGODB_URL) {
+  throw new Error('MONGODB_URLが設定されていません');
+}
+
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log('dbと接続中')
+} catch(err) {
+  console.log(err);
+}
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello, TypeScript with Express!');
+// });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
