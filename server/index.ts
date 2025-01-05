16888d40655dceedc97caf7a44536065871399b9
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-04 17:05:43 -0800

    passwordの暗号化とDBへの保存

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import CryptoJS from 'crypto-js';
+import User from './src/v1/models/user';
 import env from 'dotenv';
 env.config();
 
 const app = express();
 const port = 8080;
 
-if (!process.env.MONGODB_URL) {
-  throw new Error('MONGODB_URLが設定されていません');
+if (!process.env.MONGODB_URL || !process.env.SECRET_KEY) {
+  throw new Error('必要な環境変数が設定されていません');
 }
 
 try {
@@ -16,6 +18,17 @@ try {
 } catch(err) {
   console.log(err);
 }
+
+app.post('/register', async (req: Request, res: Response) => {
+  const password: string = req.body.password;
+
+  try {
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY as string);
+    const user = await User.create(req.body);
+  } catch(err) {
+
+  }
+});
 
 // app.get('/', (req: Request, res: Response) => {
 //   res.send('Hello, TypeScript with Express!');
