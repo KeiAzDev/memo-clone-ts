b4fa968c11fab5c68e54bd0b66dc44046b159af8
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-05 17:40:15 -0800

    jwt発行

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,52 @@
-import express, { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import CryptoJS from 'crypto-js';
-import User from './src/v1/models/user';
-import env from 'dotenv';
+import express, { Request, Response, RequestHandler } from "express";
+import mongoose from "mongoose";
+import CryptoJS from "crypto-js";
+import JWT from "jsonwebtoken";
+import User from "./src/v1/models/user";
+import env from "dotenv";
 env.config();
 
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
 if (!process.env.MONGODB_URL || !process.env.SECRET_KEY) {
-  throw new Error('必要な環境変数が設定されていません');
+  throw new Error("必要な環境変数が設定されていません");
 }
 
 try {
   mongoose.connect(process.env.MONGODB_URL);
-  console.log('dbと接続中')
-} catch(err) {
+  console.log("dbと接続中");
+} catch (err) {
   console.log(err);
 }
 
-app.post('/register', async (req: Request, res: Response) => {
+const registerHandler: RequestHandler = async (req: Request, res: Response) => {
+  //パスワードの受け取り
   const password: string = req.body.password;
 
   try {
-    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY as string);
+    //パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(
+      password,
+      process.env.SECRET_KEY as string
+    );
+    //ユーザーの新規作成
     const user = await User.create(req.body);
-  } catch(err) {
-
+    //jwt
+    const token = JWT.sign(
+      { id: user._id },
+      process.env.TOKEN_SECRET_KEY as string,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ user, token });
+  } catch (err) {
+    res.status(500).json({ error: err });
   }
-});
+};
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello, TypeScript with Express!');
-// });
+app.post("/register", registerHandler);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
